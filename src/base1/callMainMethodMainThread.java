96785ec33da24e14294078a8acb34844fd368e9c
zg666 <1095710366@qq.com>
2017-05-08 10:14:25 +0800

    21

--- src/base1/callMainMethodMainThread.java
+++ src/base1/callMainMethodMainThread.java
@@ -3,6 +3,7 @@ package base1;
 public class callMainMethodMainThread {
 	public static void main(String[] args) {
 		// 当前运行线程的名称1
+		int a=0;
 		System.out.println(Thread.currentThread().getName());
 		
 	}
